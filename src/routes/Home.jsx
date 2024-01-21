4845b0cb12eebdcb6b46d4e061ee14a4bee7a065
Deep Singh <deepsingh25244@gmail.com>
2024-01-21 18:24:35 +0530

    updats

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Crousel from '../components/Crousel'
+import Cards from '../components/Cards'
+
 
 function Home() {
   return (
-    <div>
+    <div className='h-full'>
       <Navbar/>
+      <Crousel/>
+      <Cards/>
+      <Footer/>
+
+
+
     </div>
   )
 }
